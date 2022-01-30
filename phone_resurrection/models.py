8e0d346ae0d9a3cb7db8e2177ddb214267dfb9d1
MakarovAlexeyZrck <mak_alexey@icloud.com>
2022-01-30 14:52:34 +0300

    local: 14:51 1. Сделано API по выделению номеров телефонов для проверки в Я определителе 2. Сделано APi по выделению номеров телефонов с различными типами отзывов

--- phone_resurrection/models.py
+++ phone_resurrection/models.py
@@ -5,23 +5,19 @@ from django.utils import timezone
 class PhonesReestr(models.Model):
 
     """
-        Автор:      Макаров Алексей
-        Описание:   Реестр телефонов и информация о них в Я определителе
+    Автор:      Макаров Алексей
+    Описание:   Реестр телефонов и информация о них в Я определителе
     """
 
-    phone_desc      = models.CharField(max_length = 100)
-    phone_nums      = models.CharField(max_length = 100, unique = True)
+    phone_numbs  = models.CharField(max_length = 100, unique = True)
 
-    det_polar       = models.CharField(max_length = 100, default = "check_need")
-    det_status      = models.CharField(max_length = 100, default = "check_need")
-    det_verdict     = models.CharField(max_length = 100, default = "check_need")
-    det_desc        = models.CharField(max_length = 100, default = "check_need")
+    phone_state  = models.CharField(max_length = 100, default = "check_need")
+    phone_descr  = models.CharField(max_length = 100, default = "check_need")
 
-    time_updated    = models.DateTimeField(auto_now = True)
-    time_created    = models.DateTimeField(auto_now_add = True)
+    time_updated = models.DateTimeField(auto_now = True)
+    time_created = models.DateTimeField(auto_now_add = True)
 
-    need_to_update  = models.SmallIntegerField(default = 1)
-    need_to_improve = models.SmallIntegerField(default = 0)
+    update_need  = models.SmallIntegerField(default = 1)
 
     class Meta:
         db_table = "phones_reestr"
--- phone_resurrection/urls.py
+++ phone_resurrection/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from phone_resurrection.views import PhonesReestView
-
+from phone_resurrection import views
 
 urlpatterns = [
 
-    path("resurrection/", PhonesReestView.as_view()),
-    path("resurrection/<int:id>", PhonesReestView.as_view()),
+    path(
+        "resurrection/phone_state/<str:state>",
+        views.fetch_on_phone_state
+    ),
+
+    path(
+        "resurrection/phone_update_status/<int:phone_update_status>",
+        views.fetch_on_phone_update_status
+    ),
 
 ]
