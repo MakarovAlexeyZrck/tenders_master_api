839e2c41c018c2b736bc51ec87210e31b64b218c
MakarovAlexeyZrck <mak_alexey@icloud.com>
2022-02-27 22:55:56 +0300

    strange fix

--- mail_to_supplier/urls.py
+++ mail_to_supplier/urls.py
@@ -3,10 +3,21 @@ from django.urls import path
 from mail_to_supplier.views import NotificationTemplate
 from mail_to_supplier.views import SendNotificationTemplate
 
-
+from mail_to_supplier.views import NotificationSupplierRejection
+from mail_to_supplier.views import NotificationSupplierTermination
 urlpatterns = [
 
     path("supplier/notification/<int:id>", NotificationTemplate.as_view()),
-    path("supplier/send/", SendNotificationTemplate.as_view())
+    path("supplier/send/", SendNotificationTemplate.as_view()),
+
+    path(
+        "supplier/notification/rejection/",
+        NotificationSupplierRejection.as_view()
+    ),
+
+    path(
+        "supplier/notification/termination/",
+        NotificationSupplierTermination.as_view()
+    )
 
 ]
--- mail_to_supplier/views.py
+++ mail_to_supplier/views.py
@@ -16,6 +16,77 @@ from mail_to_supplier.serializers import NotificationManagerRecipient
 from mail_to_supplier.SmtpManager import SmtpManager
 
 
+class NotificationSupplierRejection(APIView):
+
+     """
+         Автор:      Макаров Алексей
+         Описание:   Генерирование шаблона с уведомлением
+                     поставщика об уклонении / отказе от выполнения контракта
+     """
+
+     def __init__(self) -> None:
+
+         """
+             Автор:      Макаров Алексей
+             Описание:   Инициализация класса по созданию письма - уведомления
+         """
+
+         self.template = loader.get_template("rejection.html")
+
+     def post(self, request) -> Response:
+
+         """
+             Автор:      Макаров Алексей
+             Описание:   Получение шаблона письма с уведомлением поставщика
+         """
+
+         context = {
+             "supplier_name": request.POST["supplier_name"],
+             "customer_name": request.POST["customer_name"],
+             "purchase_number": request.POST["purchase_number"],
+             "contact_manager_name": request.POST["contact_manager_name"],
+             "contact_manager_email": request.POST["contact_manager_email"],
+             "contact_manager_phone": request.POST["contact_manager_phone"],
+         }
+
+         return HttpResponse(self.template.render(context, request))
+
+
+class NotificationSupplierTermination(APIView):
+
+     """
+         Автор:      Макаров Алексей
+         Описание:   Генерирование шаблона с уведомлением
+                     поставщика об уклонении / отказе от выполнения контракта
+     """
+
+     def __init__(self) -> None:
+
+         """
+             Автор:      Макаров Алексей
+             Описание:   Инициализация класса по созданию письма - уведомления
+         """
+
+         self.template = loader.get_template("termination.html")
+
+     def post(self, request) -> Response:
+
+         """
+             Автор:      Макаров Алексей
+             Описание:   Получение шаблона письма с уведомлением поставщика
+         """
+
+         context = {
+             "supplier_name": request.POST["supplier_name"],
+             "customer_name": request.POST["customer_name"],
+             "purchase_number": request.POST["purchase_number"],
+             "contact_manager_name": request.POST["contact_manager_name"],
+             "contact_manager_email": request.POST["contact_manager_email"],
+             "contact_manager_phone": request.POST["contact_manager_phone"],
+         }
+
+         return HttpResponse(self.template.render(context, request))
+
 class NotificationTemplate(APIView):
 
     """
